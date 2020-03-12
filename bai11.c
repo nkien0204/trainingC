b547f1bd0df181e5f6cf893ba1d1ad60f947081e
nkien0204 <nkien0204@gmail.com>
2020-03-12 17:19:02 +0700

    completed bai11

--- bai11.c
+++ bai11.c
@@ -1,34 +1,52 @@
-#include<stdio.h>
-#include<string.h>
+#include"proj.h"
+#include<regex.h>
+#include <dirent.h> 
 
+void Find();
+void Regex(char *r_target, char *fileName);
 
-int main() {
+int main() {  
+    Find();
+    return 0;
+}
+
+void Find() {
     FILE *fptr;
     char directory[100], target[10];
+    struct dirent *drent;
 
-    do {
-        printf("Enter directory of folder (with '/' on the end): ");
-        scanf("%s", directory);
-    } while (directory[strlen(directory) - 1] != '/');
+    printf("Enter directory of folder: ");
+    scanf("%s", directory);
+    
     printf("Enter file or folder to search: ");
     scanf("%s", target);
 
-    strcat(directory, target);
-    
-    fptr = fopen(directory, "r");
-    if (fptr != NULL) {
-        printf("Result: ");
-        for (int i = strlen(directory) - 1; i >= 0; i--) {
-        if (directory[i] == '/') {
-            for (i = i + 1; i < strlen(directory); i++) {
-                printf("%c", directory[i]);
-            }
-            break;
-        }
+    //point dir to folder
+    DIR *dir = opendir(directory);
+    if (dir == NULL) { 
+        printf("Could not open current directory" ); 
+        return; 
     }
-    } else {
-        printf("File does not exist.\n");
+    printf("Result: \n");
+    while ((drent = readdir(dir)) != NULL) {
+        Regex(target, drent->d_name);
     }
+    closedir(dir);
     printf("\n");
-    return 0;
+}
+
+void Regex(char r_target[], char *fName) {
+    regex_t regex;
+    int reti;
+    char reg_ex[10] = "^";
+    strcat(reg_ex, r_target);
+    reti = regcomp(&regex, reg_ex, 0);
+    if (reti) {
+        fprintf(stderr, "Could not compile regex\n");
+        exit(1);
+    }
+    reti = regexec(&regex, fName, 0, NULL, 0);
+    if (!reti) {
+         printf("%s\n", fName);
+    }
 }
